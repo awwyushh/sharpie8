5d0db7b428b484ea12898d070de7c5d17ae64208
ayu. <aezyus1@gmail.com>
2026-07-05 12:08:43 +0530

    feat(memory): memorytests modified and updated

--- tests/Sharpie8.Tests/MemoryTests.cs
+++ tests/Sharpie8.Tests/MemoryTests.cs
@@ -2,10 +2,21 @@ using Xunit;
 
 namespace Sharpie8.Tests;
 
-public class MemoryTests {
+public class MemoryTests
+{
     [Fact]
-    public void Memory_IsZeroInitialized(){
-	Memory memory = new();
-	Assert.Equal(0, memory.ReadByte(123));
+    public void Memory_IsZeroInitialized()
+    {
+        var memory = new Memory();
+        Assert.Equal(0, memory.ReadByte(0));
+        Assert.Equal(0, memory.ReadByte(4095));
+    }
+
+    [Fact]
+    public void WriteByte_StoresValue()
+    {
+        var memory = new Memory();
+        memory.WriteByte(0x123, 0xAB);
+        Assert.Equal(0xAB, memory.ReadByte(0x123));
     }
 }
